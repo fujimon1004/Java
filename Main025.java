700614a38bd07fb6d2a26a6e1d3e028080fbb060
fujimon1004 <fujimon1004@gmail.com>
2024-05-27 19:58:02 +0900

    inital

---
+++ Main025.java
@@ -0,0 +1,7 @@
+public class Main025 {
+    public static void main(String[] args) {
+        System.out.println("さとしです");
+        System.out.println("31さいです");
+        System.out.println(31 + 31);
+    }
+}
