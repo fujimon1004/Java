f728001477838657681551bb431f57ab644cf3a9
fujimon1004 <fujimon1004@gmail.com>
2024-05-27 22:07:25 +0900

    `P60`

---
+++ 1-5/Practice13.java
@@ -0,0 +1,15 @@
+public class Practice13 {
+    public static void main(String[] args) {
+        boolean is = true;
+        char name = '駆';
+        double value = 3.14;
+        long start = 314159265853979L;
+        String str = "HOGEHOGEHOGE";
+
+        System.out.println(is);
+        System.out.println(name);
+        System.out.println(value);
+        System.out.println(start);
+        System.out.println(str);
+    }
+}
