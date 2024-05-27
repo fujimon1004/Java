d681da4364799e8b270834e79340d19995bba678
fujimon1004 <fujimon1004@gmail.com>
2024-05-27 22:08:11 +0900

    hoge

---
+++ Practice12.java
@@ -0,0 +1,11 @@
+public class Practice12 {
+    public static void main(String[] args) {
+        int a;
+        a = 3;
+        int b;
+        b = 5;
+        int c;
+        c = a * b;
+        System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+    }
+}
